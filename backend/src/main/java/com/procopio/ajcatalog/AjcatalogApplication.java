a74153c29e862f5d3234ccb010b30c0eb7114991
Amauri Junior <apojr98@gmail.com>
2022-05-17 17:50:36 -0300

    Backend project created

---
+++ backend/src/main/java/com/procopio/ajcatalog/AjcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.procopio.ajcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjcatalogApplication.class, args);
+	}
+
+}
