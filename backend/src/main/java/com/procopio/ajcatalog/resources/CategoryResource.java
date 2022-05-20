636996d76bb907209ed8cc569e54f59bbdfa3dbe
Amauri Junior <apojr98@gmail.com>
2022-05-20 10:47:28 -0300

    DTO

--- backend/src/main/java/com/procopio/ajcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/procopio/ajcatalog/resources/CategoryResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.procopio.ajcatalog.dto.CategoryDTO;
 import com.procopio.ajcatalog.entities.Category;
 import com.procopio.ajcatalog.services.CategoryService;
 
@@ -21,8 +22,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/procopio/ajcatalog/services/CategoryService.java
+++ backend/src/main/java/com/procopio/ajcatalog/services/CategoryService.java
@@ -2,12 +2,15 @@
 
 package com.procopio.ajcatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.procopio.ajcatalog.dto.CategoryDTO;
 import com.procopio.ajcatalog.entities.Category;
 import com.procopio.ajcatalog.respositories.CategoryRepository;
 
@@ -18,7 +21,9 @@ public class CategoryService {
 	private CategoryRepository repository; 
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list =  repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
