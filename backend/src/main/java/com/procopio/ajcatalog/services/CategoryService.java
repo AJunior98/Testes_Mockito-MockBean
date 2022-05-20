ab05e94d553e440a2fed985153aed9de99f2f679
Amauri Junior <apojr98@gmail.com>
2022-05-20 12:11:26 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/procopio/ajcatalog/services/CategoryService.java
+++ backend/src/main/java/com/procopio/ajcatalog/services/CategoryService.java
@@ -2,8 +2,8 @@
 
 package com.procopio.ajcatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.procopio.ajcatalog.dto.CategoryDTO;
 import com.procopio.ajcatalog.entities.Category;
 import com.procopio.ajcatalog.respositories.CategoryRepository;
+import com.procopio.ajcatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -25,5 +26,11 @@ public class CategoryService {
 		List<Category> list =  repository.findAll();
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
