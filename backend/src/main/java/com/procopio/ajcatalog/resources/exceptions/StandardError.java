6ff9fe42eae3adaaadbcd419e243a53d3644c265
Amauri Junior <apojr98@gmail.com>
2022-06-16 13:31:58 -0300

    Refactor pageable

--- backend/src/main/java/com/procopio/ajcatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/procopio/ajcatalog/resources/exceptions/StandardError.java
@@ -1,8 +1,9 @@
 package com.procopio.ajcatalog.resources.exceptions;
 
+import java.io.Serializable;
 import java.time.Instant;
 
-public class StandardError {
+public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Instant timestamp;
